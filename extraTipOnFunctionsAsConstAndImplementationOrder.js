d513cfe8442a5319da53f162dc0f3879f0f42d4a
GManentis <manentis.gerasimos@outlook.com>
2022-04-18 11:10:11 +0300

    correcton in extra tip

--- extraTipOnFunctionsAsConstAndImplementationOrder.js
+++ extraTipOnFunctionsAsConstAndImplementationOrder.js
@@ -3,11 +3,12 @@ const Joi = require("joi");
 
 /*
 //This wont work because if declared as const, a function first must be declared before execution
+*/
 foo("jgtjtij");
 const foo = (x) => {
 	console.log(x)
 }
-*/
+/*End*/
 
 
 /*This will work properly*/
@@ -15,17 +16,11 @@ foo("jgtjtij");
 function foo(x){
 	console.log(x)
 }
+/*End*/
 
 
-/*below we will also have an error occurance because even if 
+/*below we will also have an error occurance because even if
 myparamFunction can be used the object schema has not been initialised
-*/
-
-/*
-if lines 30-35 where included as part of module to be exported, there would be no error issue:
- module.exports = function(){ 
- //lines 30-35
- }
 */
 let y = null;
 
@@ -38,3 +33,24 @@ const myParamSchema = Joi.string().required();
 function myParam(input){
 	return myParamSchema.validate(input);
 }
+
+/*End case*/
+
+/*The strange case of module exports. In the example below the exported module will work fine though its logic collides with the above cases
+The following module will work but consts are not in order, cases that would make things worse.
+*/
+module.exports = function(){
+	displayMessage();
+};
+
+const displayMessage = () => {
+	console.log(param);
+}
+
+const param = "OK";
+
+
+/*
+To sum up, always place things in proper order if you have arrow or functions returned as const/let, always use best practices and dont
+implement modules as above but always in the right order
+*/
