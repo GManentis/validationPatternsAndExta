9dde0235e45e9341b03ebe4a22ed718f0963f01b
GManentis <manentis.gerasimos@outlook.com>
2022-05-08 00:28:48 +0300

    Alteration in notes of js and php comparisons datatypes

--- javascript-comparison-datatypes.js
+++ javascript-comparison-datatypes.js
@@ -1,10 +1,16 @@
 /*
-how comparison works in js, with examples: 
+how comparison of different datatypes works in js:
+
+1. If one of the compared values is number (no quotas), convert the other member to number before comparison
+2. If boolean is compared with a variable that is number or is string of numeric value, the boolean is converted to 1 or 0
+3. If boolean is compared to non empty string it equals (with == )true else is considered false
+
+Examples: 
 */
-console.log(2 > "a"); //js checks that 2 is number and A is string. So it compares them as "2" > "A"
+console.log(2 > "a"); //js checks that 2 is number and A is string. So it compares them as 2 > NaN. NaN cannot be compared with number so it is false
 console.log(2 > 11); //both are numeric and the comparison is like numeric
 console.log("2" > "11"); //both data types are considered strings and are compared as strings. 2 is greater than
-console.log("2" > true); //boolean is treated as 1, js checks if "2" is numeric and compares them as numbers, so it is 2 > 1 which is true
+console.log("2" > true); //js checks if "2" is numeric. So, boolean is treated as 1, and compares them as numbers, so it is 2 > 1 which is true
 console.log(-2 < false); //boolean is treated as 0, and the comparison turns to be -2 < 0 which is true
 console.log(true > false); //true is 1 and false is 0 => returns true
-console.log("foo" > true); //Foo is string so is "Foo" > "true" which is false because T > F as strings
+console.log("foo" < true); // non empty string == to true, so the comparison is false
--- php-comparison-datatypes.php
+++ php-comparison-datatypes.php
@@ -1,22 +1,31 @@
 <?php
 /*
-how comparison works in php, with examples: 
+how datatype comparison works in php, 
+1. If one of the compared values is number (no quotas), convert the other member to number before comparison
+2. If two string are compared, PHP checks if both are of numeric value of start with numbers .If both are, then are compared as numbers else as strings
+
+3. If boolean is compared with a variable that is number (without quotas), if number is no zero, then it is considered true else false
+4. If boolean is compared to string, if string not empty and not equal "0" then is true. Else, false
+
+Important note, "true" in "" is considered string not boolean
+
+Examples: 
 the reason we echo boolean as strings is beacause php echos truthities as 1 and falseties as empty space
 */
 echo 4 > "A" ? "true" : "false"; //PHP tries to treat "A" as number because it is compared with a number so it treats it as 0. If there was f.e 5a, it would be treated as 5
 echo "\n";
-echo "2" > "A" ? "true" : "false"; //PHP treats both entries as strings beacause 2 in in "", so it will be false 
+echo "2" > "A" ? "true" : "false"; //PHP treats both entries as numbers, so it will be false 
 echo "\n";
-echo 2 > 11 ? "true" : "false"; //both are numeric and the comparison is like numeric
+echo 2 > 11 ? "true" : "false"; //both are numeric and the comparison is like numeric.False
 echo "\n";
-echo "2" > "11" ? "true" : "false"; //PHP treats both as numbers due to their values inside the ""
+echo "2" < "11" ? "true" : "false"; //PHP treats both as numbers due to their values inside the "".The condition is true
 echo "\n";
-echo "2" > true ? "true" : "false"; // PHP checks value of "2", finds out that is a number, number other than 0 is true so it is true > true which is false 
+echo "2" > true ? "true" : "false"; // Not empty strings and not equal to "0" are true, else false. So true > true means false
 echo "\n";
-echo -2 < false ? "true" : "false"; //Numbers other than 0 are true so the comparison is true < false which is false
+echo -2 > false ? "true" : "false"; //Numbers when compared to true of false are considered true if they are not 0. Else are considered false. So here is true > false which is true
 echo "\n";
 echo true > false ? "true" : "false"; //true > false, can be considered self explanatory
 echo "\n";
-echo "Foo" > true ? "true" : "false"; //Foo is string so is "Foo" > "true" which is false because T > F as strings
-/*Important note, "true" in "" is considered string not boolean*/
+echo "Foo" < true ? "true" : "false"; // Non empty string and not equal to "0" when compared to boolean as considered true else are considered false. So here true < true, which is false 
+
 ?>
